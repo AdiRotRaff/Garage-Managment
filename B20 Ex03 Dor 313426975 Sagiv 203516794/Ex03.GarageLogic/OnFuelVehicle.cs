992fb9b48655618c104d3f610983a1efda855bad
Dor david Lasri <lasridor@gmail.com>
2020-06-02 11:53:20 +0300

    first arrangment

---
+++ B20 Ex03 Dor 313426975 Sagiv 203516794/Ex03.GarageLogic/OnFuelVehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public abstract class OnFuelVehicle
+    {
+        private enum eFuelType
+        {
+            Octan95 = 0,
+            Octan98,
+            Soler
+        }
+    }
+}
---
+++ B20 Ex03 Dor 313426975 Sagiv 203516794/Ex03.GarageLogic/Vehicles.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public abstract class Vehicles
+    {
+        private readonly string r_ModelName;
+        private readonly string r_LicenceNumber;
+        private float m_PrecentageOfRemainingEnergy;
+        private Wheel m_CollectionOfWheels;
+
+        public struct Wheel
+        {
+            private readonly string r_ManufactorName;
+            private readonly float r_MaxAirPressure;
+            private float m_CurrAirPressure;
+
+            public Wheel(string i_ManufactorName, float i_MaxAirPressure, float i_CurrAirPressure)
+            {
+                this.r_ManufactorName = i_ManufactorName;
+                this.r_MaxAirPressure = i_MaxAirPressure;
+                this.m_CurrAirPressure = i_CurrAirPressure;
+            }
+
+            public void BlowTheWheel(float i_VolumeOfAir)
+            {
+                if (m_CurrAirPressure + i_VolumeOfAir > r_MaxAirPressure)
+                {
+                    m_CurrAirPressure = r_MaxAirPressure;
+                }
+                else
+                {
+                    m_CurrAirPressure = m_CurrAirPressure + i_VolumeOfAir;
+                }
+            }
+
+            public string ManufactorName
+            {
+                get { return r_ManufactorName; }
+            }
+
+            public float MaxAirPressure
+            {
+                get { return r_MaxAirPressure; }
+            }
+
+            public float CurrAirPressure
+            {
+                get { return m_CurrAirPressure; }
+            }
+        }
+}
